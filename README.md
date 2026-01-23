2147ac9a2151e76291e95dcac18cc25dc2c23978
313ctrlcrunn3r <leotocca99+github.com@googlemail.com>
2026-01-23 16:51:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Corso fondamenti di programmazione
+
+Insieme di esercizi in python
---
+++ python_test/main.py
@@ -0,0 +1,27 @@
+# lol
+#print("Inserisci frase da colorare:")
+RESET = "\x1b[0m"
+#frase = input()
+# \x1b[38;2;200;200m] lilla
+LILLA = "\x1b[38;2;221;160;221m"
+GIALLO_BG="\x1b[48;2;229;190;1m"
+ROSSO = "\x1b[38;2;255;0;0m"      # Rosso
+VERDE = "\x1b[38;2;0;255;0m"       # Verde
+BLU = "\x1b[38;2;0;0;255m"         # Blu
+GIALLO = "\x1b[38;2;255;255;0m"    # Giallo
+CIANO = "\x1b[38;2;0;255;255m"     # Ciano
+MAGENTA = "\x1b[38;2;255;0;255m"   # Magenta
+ARANCIONE = "\x1b[38;2;255;165;0m" # Arancione
+VERDE_LIMONE = "\x1b[38;2;204;255;0m"  # Verde Limone
+GRIGIO = "\x1b[38;2;128;128;128m"  # Grigio
+#print("\x1b[32m" + "\n" +frase)
+#print(lilla + "\n" +frase)
+#print(f"{GIALLO_BG}\n{frase} {RESET}{LILLA}{frase} no colore")
+print("inserisci il primo numero:")
+n1 = input("primo numero: ")
+print(f"Il primo numero è: {ROSSO}{n1}{RESET}")
+print("inserisci il secondo numero:")
+n2 = input("secondo numero: ")
+print(f"Il secondo numero è: {CIANO}{n2}{RESET}")
+somma = int(n1) + int(n2)
+print(f"La somma dei numeri è: {MAGENTA}{somma}")
